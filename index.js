15f4fd31751a0509cf72c6aab4623d948db6869d
ayline <ayline1695@hotmail.com>
2020-11-21 09:30:47 +0100

    no done

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=>{
+      return a-b
+    });
+    this.length = this.items.length 
+    // lo llama de nuevo para que se actualice (ayuda Tomas)
+  }
 
-  get(pos) {}
+  get(pos) {
+    let pos = 0;
+if(pos<this.length){
+    for (pos = 0; pos < this.length; pos++) {
+      this.length.add(pos);
+    }
+}
+else{
+    throw new Error('OutOfBounds');
+}
+  }
 
   max() {}
 
@@ -13,5 +33,6 @@ class SortedList {
 
   avg() {}
 }
-
+const list= new SortedList()
+list.add(2)
 module.exports = SortedList;
